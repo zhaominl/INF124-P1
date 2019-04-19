eeadf46ac9d75b62699794e6f2cfdd74e317d8e3
Zhaomin Li <zhaominl@uci.edu>
2019-04-18 19:49:16 -0700

    add home page

---
+++ script/data.js
@@ -0,0 +1,138 @@
+const CHOCOLATE_DATA = {
+    choco_1: {
+        img: "https://cdn.yamibuy.net/item/0408b56a4e2e4b46fc7f16ce270c4dc2_265x265.webp",
+        name: "LOTTE KOALA MINI COOKIE Chocolate Filling 10pck 195g Family Size",
+        price: 4.99,
+        origin: "Japan",
+        direction: "Ready to eat",
+        pic_1: "https://cdn.yamibuy.net/item/df38b96b6a79e6aec66c24d1a1582639_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/bc12a5bd98f651c775940df4d0698333_625x625.webp"
+    },
+    choco_2: {
+        img: "https://cdn.yamibuy.net/item/ed4717eeee3ff00052e4fc730dc86847_265x265.webp",
+        name: "GLICO Matcha Chocolate Pocky Stick 70g",
+        price: 1.99,
+        origin: "Japan",
+        direction: "Mocha flavor stick snack",
+        pic_1: "https://cdn.yamibuy.net/item/4269ed194f74a4527180435069942ac1_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/a3f7669aff2e5064ccd459d3b4804663_625x625.webp"
+    },
+    choco_3: {
+        img: "https://cdn.yamibuy.net/item/c86720d5e46cc334256efe22d27fbaea_265x265.webp",
+        name: "MORINAGA Dars White Chocolate Confectionery 42g",
+        price: 1.79,
+        origin: "Japan",
+        direction: "White Cream Chocolate",
+        pic_1: "https://cdn.yamibuy.net/item/e602ccf2fa589e26c27243d7fa53b877_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/bd85c3b1bf50e99d4abdefc4d659d767_625x625.webp"
+    },
+    choco_4: {
+        img: "https://cdn.yamibuy.net/item/37d1bbc8a91a3b80583bb1f1c653319c_265x265.webp",
+        name: "GLICO Cookies Chocolate Covered Biscuit Sticks Family Pack 120g",
+        price: 3.39,
+        origin: "Japan",
+        direction: "Big pack of Pocky, giving you the enjoy of taste",
+        pic_1: "https://cdn.yamibuy.net/item/32a53fa7b73801ea2603f7c04e4fa6d3_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/c53868bd5cbd3e04bee8c806043efc19_625x625.webp"
+    },
+    choco_5: {
+        img: "https://cdn.yamibuy.net/item/2843a2092cc39fdfcb4a64b3a5ab455e_265x265.webp",
+        name: "MEIJI Macadamia Chocolate 64g",
+        price: 3.49,
+        origin: "Japan",
+        direction: "Macadamia fruit, vegetable oil, white sugar, cocoa powder",
+        pic_1: "https://cdn.yamibuy.net/item/7e7787906d7c3cbd34c35f90bd48e4cd_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/c5239a705c17c0adddc5e9f5f218e665_625x625.webp"
+    },
+    choco_6: {
+        img: "https://cdn.yamibuy.net/item/136fc9ec5f6b2ede03da201ac5eccfd0_265x265.webp",
+        name: "MEIJI Meltyblend Greentea Chocolate 60g",
+        price: 3.99,
+        origin: "Japan",
+        direction: "Green Tea Flavor, Sugar, vegetable oil, cocoa, tea, etc.",
+        pic_1: "https://cdn.yamibuy.net/item/23950b25d6062b5da6785c37a0675acb_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/d2d43280a3811b91a6bbf7e835235ee7_625x625.webp"
+    },
+    choco_7: {
+        img: "https://cdn.yamibuy.net/item/9754499e341ba87e1ff2f98ed4c1bb55_265x265.webp",
+        name: "MEIJI Panda Chocolate Filling Cookie 57g",
+        price: 1.19,
+        origin: "Japan",
+        direction: "Wheat flour, palm oil, milk powder",
+        pic_1: "https://cdn.yamibuy.net/item/126bcb9a8f9eacbc541e76e9e6895fca_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/792658bad33bbe5364e59cc4a4fc7413_625x625.webp"
+    },
+    choco_8: {
+        img: "https://cdn.yamibuy.net/item/36adb2c93acf66d46fa56b12521e033b_265x265.webp",
+        name: "MEIJI Meltyblend Chocolate (Premium Cacao) 60g",
+        price: 3.99,
+        origin: "Japan",
+        direction: "This limited edition chocolate is made from premium cocoa beans to enhance the deep, chocolatey flavor. The dark chocolate is not too sweet and leaves a lingering taste of rich cocoa cream on the tongue. Each cube is individually wrapped to seal in the freshness of each piece and improve portability.",
+        pic_1: "https://cdn.yamibuy.net/item/c1c760311d70a65336fc78d6cc1809c5_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/c164dee229bcfadd46bd3488b5c1024a_625x625.webp"
+    },
+    choco_9: {
+        img: "https://cdn.yamibuy.net/item/8b42bf87f94c053858de296d5d45e21e_265x265.webp",
+        name: "MORINAGA Dars Silky Milk Chocolate42g",
+        price: 1.79,
+        origin: "Japan",
+        direction: "Silky chocolate that melts instantly in your mouth.",
+        pic_1: "https://cdn.yamibuy.net/item/36e32c8e9f6dd34e78e956a3b266cd2c_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/2f1e935af3c898c05fcd47af01ac6deb_625x625.webp"
+    },
+    choco_10: {
+        img: "https://cdn.yamibuy.net/item/fd584cfb76f67fa7efa1bbd1dffabf7f_265x265.webp",
+        name: "GLICO Pejoy Chocolate Cream Filled Biscuit Sticks 56g",
+        price: 1.99,
+        origin: "Japan",
+        direction: "Wheat flour, vegetable oil, sugar, whole milk powder",
+        pic_1: "https://cdn.yamibuy.net/item/0cabec104211f4802549e237989a0594_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/0c6e484892685255e3066aec86ba2680_625x625.webp"
+    },
+    choco_11: {
+        img: "https://cdn.yamibuy.net/item/91a92c18f344c7d23546fa2d4aedf401_265x265.webp",
+        name: "MEIJI Chocolate Gummy Candy Muscat Flavor 81g",
+        price: 3.99,
+        origin: "Japan",
+        direction: "MEIJI Chocolate Gummy Candy is a popular Japanese sweet treat, these are round chocolate covered gummy pieces. The handy tube is filled with chewy Grape flavored gummy pieces covered in a rich chocolate shell. Yum!",
+        pic_1: "https://cdn.yamibuy.net/item/209647556914b7ffd6bbdc0fb99ac177_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/7743202e00e2b7abca22d3e2688cf3c1_625x625.webp"
+    },
+    choco_12: {
+        img: "https://cdn.yamibuy.net/item/2a53fa8039ae3c649b0739d36f0b276e_265x265.webp",
+        name: "LOTTE PEPERO Stick Biscuit & Chocolate 47g",
+        price: 1.19,
+        origin: "Japan",
+        direction: "Wheat flour, sugar, cocoa butter, vegetable oil, ghee, whole milk powder",
+        pic_1: "https://cdn.yamibuy.net/item/7a193ae27a69f297f7a688e623461683_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/85f81961c018f169b92431ffd6b73922_625x625.webp"
+    },
+    choco_13: {
+        img: "https://cdn.yamibuy.net/item/3efa4bc90f366ccdd2015032e150e256_265x265.webp",
+        name: "GLICO Classic Chocolate Pocky Stick Family Pack 9 Packs",
+        price: 3.49,
+        origin: "Japan",
+        direction: "Chocolate Flavor",
+        pic_1: "https://cdn.yamibuy.net/item/8965a30780f49bc46318ffb9f28de856_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/4ce0976a78f622178fb0572ae77c62fc_625x625.webp"
+    },
+    choco_14: {
+        img: "https://cdn.yamibuy.net/item/2caddfb63c47fc669311dc0b6ad343c3_265x265.webp",
+        name: "GLICO Super Slender Chocolate Pocky 73g",
+        price: 1.89,
+        origin: "Japan",
+        direction: "Super Slender Chocolate Flavor	",
+        pic_1: "https://cdn.yamibuy.net/item/fcc35f083f00cab8b5eb6eb3fdedc595_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/6dd325f62f2b0e7c3793bc0685141c3a_625x625.webp"
+    },
+    choco_15: {
+        img: "https://cdn.yamibuy.net/item/6eda69bb4510ad4be5579f010c545000_265x265.webp",
+        name: "GLICO PEJOY Creamy Vanilla Filled Chocolate Biscuit Sticks 56g",
+        price: 1.99,
+        origin: "Japan",
+        direction: "GLICO PEJOY Creamy Vanilla Filled Chocolate Biscuit Sticks is the classic creamy flavor inside a biscuit stick for a new snacking experience. Delicious alone or with coffee, tea, milk, or wine, Pocky Chocolate is the perfect every-day, all-occasion chocolate snack.",
+        pic_1: "https://cdn.yamibuy.net/item/a8f1d2ce867f25a2f7674ae642525b8b_625x625.webp",
+        pic_2: "https://cdn.yamibuy.net/item/50f69df6edf1d576bd1479366f4f0fcd_625x625.webp"
+    }
+
+}
