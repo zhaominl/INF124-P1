e6f3a910aa0234028c8318f5e331009f723d5a1a
Zhaomin Li <zhaominl@uci.edu>
2019-04-21 23:11:21 -0700

    add mailing

--- script/item.js
+++ script/item.js
@@ -6,9 +6,24 @@ var urlParams = new URLSearchParams(window.location.search);
 
 var num = urlParams.get("num");
 
+function purchase_item(){
+    send_email('niuniu1397@gmail.com');
+}
+
+function send_email(user_email){
+    var email_url = "mailto:";
+    email_url += user_email + "?subject=" + encodeURIComponent("Order Confirmation") +
+        "&body=" + encodeURIComponent("Thank you for purchase "+CHOCO_DATA["choco_"+num].name);
+    
+    window.open(email_url,'popUpWindow','height=400,width=600,left=10,top=10,,scrollbars=yes,menubar=no');
+}
 
 window.onload = function (){
     document.getElementById("main-picture").src = CHOCO_DATA["choco_"+num].img;
     document.getElementById("add-picture-1").src = CHOCO_DATA["choco_"+num].pic_1;
     document.getElementById("add-picture-2").src = CHOCO_DATA["choco_"+num].pic_2;
+}
+
+document.getElementById('purchase-button').onclick = function(){
+    purchase_item();
 }
